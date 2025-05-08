cb3336b76e674a08e3cb9906a2a4d8f4dcd7110e
AK-10 <atsushi524k10@gmail.com>
2025-05-08 21:41:18 +0900

    sl

--- abc/0404/a_not_found/target/debug/deps/a_not_found-697db580c145fd78.d
+++
@@ -1,5 +0,0 @@
-/home/ak-10/works/atcoder/abc/0404/a_not_found/target/debug/deps/liba_not_found-697db580c145fd78.rmeta: src/main.rs
-
-/home/ak-10/works/atcoder/abc/0404/a_not_found/target/debug/deps/a_not_found-697db580c145fd78.d: src/main.rs
-
-src/main.rs:
--- abc/0404/a_not_found/target/debug/deps/a_not_found-e66e145a116856be.d
+++
@@ -1,5 +0,0 @@
-/home/ak-10/works/atcoder/abc/0404/a_not_found/target/debug/deps/a_not_found-e66e145a116856be: src/main.rs
-
-/home/ak-10/works/atcoder/abc/0404/a_not_found/target/debug/deps/a_not_found-e66e145a116856be.d: src/main.rs
-
-src/main.rs:
